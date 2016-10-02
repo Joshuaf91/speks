25533aa94964d64517dd382d772832ce9fbdde57
victoramosjr <victoramosjr@gmail.com>
2016-10-02 16:36:28 -0400

    Updated index.html to link bootstrap node module and updated footer.js file to correct className syntax error.

--- src/footer.js
+++ src/footer.js
@@ -6,10 +6,20 @@ var Footer = React.createClass({
 	render: function() {
 		return(
 			<div>
-        		<div class="row">
-  					<div class="col-sm-4">COLUMN 1 TEST</div>
-  					<div class="col-sm-4">COLUMN 2 TEST</div>
-  					<div class="col-sm-4">COLUMN 3 TEST</div>
+        		<div className="row">
+  					
+  					<div className="col-sm-4">
+  						Follow Us On Twitter!
+  					</div>
+
+  					<div className="col-sm-4">
+  						&copy; SPEKS 2015 - {new Date().getFullYear()}
+  					</div>
+
+  					<div className="col-sm-4">
+  						Subscribe To Our Mailing List
+  					</div>
+
 				</div>
 			</div>
 		)
