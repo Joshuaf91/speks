9f87b294c1216c16284ec7242e9d55930f71a547
victoramosjr <victoramosjr@gmail.com>
2016-10-02 16:06:37 -0400

    (1) Added script link to index.html (2) Commented out some import lines in order to get functioning environment for nmp compiler in App.js file. (3) Updated code in Footer.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Home from "./home.jsx";
-import './individual-product-page.js';
-import data from './data';
+// import Home from "./home.jsx";
+// import './individual-product-page.js';
+// import data from './data';
 import Nav from './Nav'; 
 import Footer from './Footer';
 
@@ -13,7 +13,6 @@ var App = React.createClass({
     return (
       <div>
       	<Nav />
-        <Home />
         <Footer />
       </div>
     )
--- src/footer.js
+++ src/footer.js
@@ -1,13 +1,16 @@
 var React = require('react');
+import 'bootstrap/dist/css/bootstrap.css';
 
 var Footer = React.createClass({
 
 	render: function() {
 		return(
 			<div>
-				<h1>
-					Footer Test Text
-				</h1>
+        		<div class="row">
+  					<div class="col-sm-4">COLUMN 1 TEST</div>
+  					<div class="col-sm-4">COLUMN 2 TEST</div>
+  					<div class="col-sm-4">COLUMN 3 TEST</div>
+				</div>
 			</div>
 		)
 	}
