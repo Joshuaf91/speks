21cc9ed0b23427756486f2e7051c9815de1ffad9
Nyapal <nyapal14@gmail.com>
2016-10-03 12:40:44 -0400

    Deleted Products from Home page

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,65 +1,10 @@
 import React from 'react';
 
-
 var Home = React.createClass({
   render: function() {
     return (
       <div>
-        HEAD
-      	<h1 className="text-center">HEADER</h1>
-        <h1 className="text-center"> speks </h1>  
-        <div className="row">
-            <div className="col-xs-12">
-                <img src="https://i.warbycdn.com/v/c/assets/eyeglasses-sunglasses/image/Fall-2016-eyeglasses-desktop/0/1a50f7bdf7.jpg?quality=80&width=1300" alt="Speks"/> 
-            </div>
-        </div>    
         <img id="home-image" src="https://i.warbycdn.com/v/c/assets/eyeglasses-sunglasses/image/Fall-2016-eyeglasses-desktop/0/1a50f7bdf7.jpg?quality=80&width=1300" alt="Speks"/>
-        <h3>Product</h3>
-        	<div className='row'>
-        		<div className='col-xs-4'>
-        			<img src="http://static.zennioptical.com/image/common/WomensGlassesdropdown.jpg" alt="Glasses" /> 
-        			<p className="text-center">Description - Price</p>
-        		</div>
-        		<div className='col-xs-4'>
-        			<img src="http://static.zennioptical.com/image/common/WomensGlassesdropdown.jpg" alt="Glasses" />
-        			<p className="text-center">Description - Price</p>
-        		</div>
-        		<div className='col-xs-4'>
-        			<img src="http://static.zennioptical.com/image/common/WomensGlassesdropdown.jpg" alt="Glasses" />
-        			<p className="text-center">Description - Price</p>
-        		</div>
-        	</div>
-        	<div className='row'>
-        		<div className='col-xs-4'>
-        			<img src="http://static.zennioptical.com/image/common/WomensGlassesdropdown.jpg" alt="Glasses" />
-        			<p className="text-center">Description - Price</p>
-        		</div>
-        		<div className='col-xs-4'>
-        			<img src="http://static.zennioptical.com/image/common/WomensGlassesdropdown.jpg" alt="Glasses" />
-        			<p className="text-center">Description - Price</p>
-        		</div>
-        		<div className='col-xs-4'>
-        			<img src="http://static.zennioptical.com/image/common/WomensGlassesdropdown.jpg" alt="Glasses" />
-        			<p className="text-center">Description - Price</p>
-        		</div>
-        	</div>
-        	<div className='row'>
-        		<div className='col-xs-4'>
-        			<img src="http://static.zennioptical.com/image/common/WomensGlassesdropdown.jpg" alt="Glasses" />
-        			<p className="text-center">Description - Price</p>
-        		</div>
-        		<div className='col-xs-4'>
-        			<img src="http://static.zennioptical.com/image/common/WomensGlassesdropdown.jpg" alt="Glasses" />
-        			<p className="text-center">Description - Price</p>
-        		</div>
-        		<div className='col-xs-4'>
-        			<img src="http://static.zennioptical.com/image/common/WomensGlassesdropdown.jpg" alt="Glasses" />
-        			<p className="text-center">Description - Price</p>
-        		</div>
-        	</div>
-        	
-        <hr />
-        <h1 className="text-center">FOOTER</h1>
       </div>
     )
   }
