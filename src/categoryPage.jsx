661485e68c7dfa16d30e9f49b6bd7702eb182aaa
Esmeralda <esmeralda.re@outlook.com>
2016-10-03 12:59:51 -0400

    adding category page

---
+++ src/categoryPage.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import products from './data.js';
+import 'bootstrap/dist/css/bootstrap.css';
+
+var CategoryPage = React.createClass({
+	render(){
+			return(
+					<div>
+							<div className='row'>
+								<div className='col-xs-2'>hello</div>
+								<div className='col-md-5'>how</div>
+								<div className='col-md-5'>are you</div>
+							</div>
+					</div>
+				)
+	}
+})
+
+
+
+export default CategoryPage;
