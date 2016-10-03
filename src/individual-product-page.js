f35f4f5e4cdede390494284c8ae21006a048d3fb
Esmeralda <esmeralda.re@outlook.com>
2016-10-03 12:30:59 -0400

    updated product component

--- src/individual-product-page.js
+++ src/individual-product-page.js
@@ -8,20 +8,23 @@ var ProductPage = React.createClass({
 		return{
 			products: products,
 			heading:products[0][0].productName,
-			picture: products[0][0].imgSrc[0]
+			picture: products[0][0].imgSrc[0],
+			details: products[0][0].description,
+			price: products[0][0].price
 		}
 	},
 
 	render(){
 		return(
 			<div>
-				<h3>{this.state.heading}</h3>
-				<img src={this.state.picture} alt="#"/>
-				<button>Description</button>
-				<h3>{products.productName}</h3>
-				<img src={products.imgSrc} alt="#"/>
-				<button>Details</button>
-				<button>Price</button>
+				<div className='container'>
+					<div className='row'><h3 className='col-xs-12'>{this.state.heading}</h3></div>
+					<div className='row'><img className='col-xs-12' src={this.state.picture} alt="#"/></div>
+					<div className='row'>
+						<div className='col-sm-7'><p>Description {this.state.details}</p></div>						
+						<div className='col-sm-5'><p>Price {this.state.price}</p></div>				
+					</div>
+				</div>
 			</div>
 		)
 	}
