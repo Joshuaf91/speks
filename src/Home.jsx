c927a2563c022eaaa053ba148d1484a786766e16
Nyapal <nyapal14@gmail.com>
2016-10-03 12:11:19 -0400

    updating

--- src/Home.jsx
+++ src/Home.jsx
@@ -5,19 +5,15 @@ var Home = React.createClass({
   render: function() {
     return (
       <div>
-<<<<<<< HEAD
+        HEAD
       	<h1 className="text-center">HEADER</h1>
-      	<hr />
         <h1 className="text-center"> speks </h1>  
         <div className="row">
             <div className="col-xs-12">
                 <img src="https://i.warbycdn.com/v/c/assets/eyeglasses-sunglasses/image/Fall-2016-eyeglasses-desktop/0/1a50f7bdf7.jpg?quality=80&width=1300" alt="Speks"/> 
             </div>
-        </div>
-=======
-    
-        <img id="home-image" src="https://i.warbycdn.com/v/c/assets/eyeglasses-sunglasses/image/Fall-2016-eyeglasses-desktop/0/1a50f7bdf7.jpg?quality=80&width=1300" alt="Speks"/> {/* I really wanted for this to be a slider & not just a singular image, still working on figuring out the slider, if any one knows how to do so it would be helpful, until then I'll keep working on it!*/}
->>>>>>> 3ac96545a3bfdbfdb27e87ba53584dbb6f6c44d9
+        </div>    
+        <img id="home-image" src="https://i.warbycdn.com/v/c/assets/eyeglasses-sunglasses/image/Fall-2016-eyeglasses-desktop/0/1a50f7bdf7.jpg?quality=80&width=1300" alt="Speks"/>
         <h3>Product</h3>
         	<div className='row'>
         		<div className='col-xs-4'>
