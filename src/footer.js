8d7ae39419b582d0a66f20f236347e525f7f055b
victoramosjr <victoramosjr@gmail.com>
2016-10-03 01:02:40 -0400

    Updated navigation bar

--- src/footer.js
+++ src/footer.js
@@ -5,23 +5,25 @@ var Footer = React.createClass({
 
 	render: function() {
 		return(
-			<div>
-        		<div className="row">
+			<footer className="footer">
+				<div className="container">
+        			<div className="row">
   					
-  					<div className="col-sm-4">
-  						Follow Us On Twitter!
-  					</div>
+	  					<div className="col-sm-4">
+	  						Follow Us On Twitter!
+	  					</div>
 
-  					<div className="col-sm-4">
-  						&copy; SPEKS 2015 - {new Date().getFullYear()}
-  					</div>
+	  					<div className="col-sm-4">
+	  						&copy; SPEKS 2015 - {new Date().getFullYear()}
+	  					</div>
 
-  					<div className="col-sm-4">
-  						Subscribe To Our Mailing List
+	  					<div className="col-sm-4">
+	  						Subscribe To Our Mailing List
+  						</div>
+  						
   					</div>
-
 				</div>
-			</div>
+			</footer>
 		)
 	}
 });
--- src/nav.js
+++ src/nav.js
@@ -5,9 +5,64 @@ var Nav = React.createClass({
 	render: function() {
 		return(
 			<div>
-				<h1>
-					Navigation Test Text
-				</h1>
+				<nav className="navbar navbar-default">
+					<div className="container-fluid">
+						
+						<div className="navbar-header">
+							<a className="navbar-brand nav-logo" href="#">
+								SPEKS
+							</a>
+						</div>
+
+						<ul className="nav navbar-nav">
+
+							<li>
+								<a className="nav-non-binary" href="#">
+									Non-Binary
+								</a>
+							</li>
+
+							<li>
+								<a className="nav-women" href="#">
+									Women
+								</a>
+							</li>
+
+							<li>
+								<a className="nav-men" href="#">
+									Men
+								</a>
+							</li>
+
+						</ul>
+					</div>
+				</nav>
+
+				<div className="container-fluid">
+					<div className="row second-nav-bar">
+
+						<div className="col-lg-1 non-binary-nav-bar-metal">
+	  						METAL
+	  					</div>
+
+	  					<div className="col-lg-1 non-binary-nav-bar-plastic">
+	  						PLASTIC
+	  					</div>
+
+	  					<div className="col-lg-1 non-binary-nav-bar-wood">
+	  						WOOD
+  						</div>
+
+  						<div className="col-lg-1 non-binary-nav-bar-tortoiseshell">
+	  						TORTOISE
+  						</div>
+
+  						<div className="col-lg-1 non-binary-nav-bar-multicolor">
+	  						MULTICOLOR
+  						</div>
+
+					</div>
+				</div>
 			</div>
 		)
 	}
