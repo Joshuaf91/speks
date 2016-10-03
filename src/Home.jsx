064c7cad59ec41fa09051b736def9b490331635f
Nyapal <nyapal14@gmail.com>
2016-10-03 12:03:00 -0400

    Edited home page image

--- src/Home.jsx
+++ src/Home.jsx
@@ -7,8 +7,12 @@ var Home = React.createClass({
       <div>
       	<h1 className="text-center">HEADER</h1>
       	<hr />
-        <h1 className="text-center"> speks </h1> {/* In terms of design, I wasn't sure wether the name would go in the header or below it */}
-        <img src="https://i.warbycdn.com/v/c/assets/eyeglasses-sunglasses/image/Fall-2016-eyeglasses-desktop/0/1a50f7bdf7.jpg?quality=80&width=1300" alt="Speks"/> {/* I really wanted for this to be a slider & not just a singular image, still working on figuring out the slider, if any one knows how to do so it would be helpful, until then I'll keep working on it!*/}
+        <h1 className="text-center"> speks </h1>  
+        <div className="row">
+            <div className="col-xs-12">
+                <img src="https://i.warbycdn.com/v/c/assets/eyeglasses-sunglasses/image/Fall-2016-eyeglasses-desktop/0/1a50f7bdf7.jpg?quality=80&width=1300" alt="Speks"/> 
+            </div>
+        </div>
         <h3>Product</h3>
         	<div className='row'>
         		<div className='col-xs-4'>
