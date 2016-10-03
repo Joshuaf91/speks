7defb559409ef8c484124140e2f535de93bc7683
Joshua Fermin <Joshuaf91@gmail.com>
2016-10-03 17:32:43 -0400

    fixed errors in categoryPage.js

--- src/categoryPage.jsx
+++ src/categoryPage.jsx
@@ -4,34 +4,32 @@ import products from './data.js';
 var CategoryPage = React.createClass({
 	
 	render(){
-	var newArr = products.map((element,index) => {
-		if(element.gender.indexOf(this.props.params.gender) > -1){
-			return index
-		}else{
-			return undefined
-		}
-	});
-	if (this.props.params.category) {
-		newArr = newArr.clean(undefined).map((element,index) => {
-			if(element.material.indexOf(this.props.params.gender) > -1){
-				return <div className='col-md-5'> </div>
+		var newArr = products.map((element,index) => {
+			if(element.gender.indexOf(this.props.params.gender) > -1){
+				return index
 			}else{
-				return <div className='col-md-5'> </div>
+				return undefined
 			}
-		})
-	} else {}
-		console.log(this.props.params);
+		});
+		newArr = newArr.clean(undefined).map((element,index) => {
+			if(products[element].material.indexOf(this.props.params.gender) > -1){
+				return <div className='col-md-5'></div>
+			}else{
+				return <div className='col-md-5'><img src={products[element].imgSrc[0]} alt="gaphas" /></div>
+			}
+		});
+		console.log(newArr);
 			return(
 					<div>
 							<div className='container-fluid'>
-								<div className='col-xs-2'>
+								<div className='col-xs-2 col-md-2 col-xl-2'>
 									<h5>Sort By:</h5>
 									<ul>
 										<li>Price</li>
 										<li>Popularity</li>
 									</ul>
 								</div>
-								<div className='col-md-10'>
+								<div className='col-xs-10 col-md-10 col-xl-10'>
 									{newArr}
 								</div>
 							</div>
